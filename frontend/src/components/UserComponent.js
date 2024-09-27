42e917fb05c8c71d70684bfdd1e27020996e3724
Oleg Ponomarenko <oleg@code-care.pro>
2024-09-27 16:16:38 +0300

    Initial front

---
+++ frontend/src/components/UserComponent.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const UserComponent = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/users')
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the users!', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name} ({user.email})</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default UserComponent;
