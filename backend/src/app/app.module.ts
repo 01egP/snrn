8511e74468fc359880949ac002d974e0ce91623b
Oleg Ponomarenko <01egP@users.noreply.github.com>
2024-10-12 16:43:19 +0300

    Create auth backend

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { configService } from '../config/config.service';
 import { UserModule } from '../user/user.module';
+import { AuthModule } from '../auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
@@ -19,6 +20,7 @@ import { join } from 'path';
     }),
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()), // Use the config service for DB configuration
     UserModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Post,
+  Body,
+  Request,
+  UseGuards,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(
+    @Body('name') name: string,
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    return this.authService.register(name, email, password);
+  }
+
+  @Post('login')
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    const user = await this.authService.validateUser(email, password);
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    return this.authService.login(user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
