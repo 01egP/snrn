01b6753cbea111eaa53b00cb6b8005633f0125cb
Oleg Ponomarenko <01egP@users.noreply.github.com>
2024-10-28 18:47:33 +0200

    Create category

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { configService } from '../config/config.service';
 import { UserModule } from '../user/user.module';
 import { AuthModule } from '../auth/auth.module';
+import { CategoryModule } from '../category/category.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
@@ -17,6 +18,7 @@ import { join } from 'path';
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()), // Use the config service for DB configuration
     UserModule,
     AuthModule,
+    CategoryModule,
   ],
   controllers: [AppController],
   providers: [AppService],
