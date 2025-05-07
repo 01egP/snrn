9247d5595f1764878bef62c1a4d4086f763dc3a7
Oleg Ponomarenko <01egP@users.noreply.github.com>
2025-05-07 17:15:01 +0300

    Add Swagger to category

--- backend/src/category/dto/create-category.dto.ts
+++ backend/src/category/dto/create-category.dto.ts
@@ -1,6 +1,9 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCategoryDto {
+  @ApiProperty({ example: 'Food' })
   @IsNotEmpty()
+  @IsString()
   name: string;
 }
