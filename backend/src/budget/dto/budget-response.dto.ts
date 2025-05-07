fc9dd3533e0444f105b7b43b77e58a8303d4e1c7
Oleg Ponomarenko <01egP@users.noreply.github.com>
2025-05-07 17:24:17 +0300

    Add Swagger to budget

--- backend/src/budget/dto/budget-response.dto.ts
+++ backend/src/budget/dto/budget-response.dto.ts
@@ -1,24 +1,18 @@
-import { IsNotEmpty, IsNumber } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
-export class CreateBudgetDto {
+export class BudgetResponseDto {
+  @ApiProperty({ example: 1 })
+  id: number;
+
   @ApiProperty({ example: 2 })
-  @IsNotEmpty()
-  @IsNumber()
   categoryId: number;
 
   @ApiProperty({ example: 1000.0 })
-  @IsNotEmpty()
-  @IsNumber()
   amount: number;
 
   @ApiProperty({ example: 5, description: 'Month as number (1–12)' })
-  @IsNotEmpty()
-  @IsNumber()
   month: number;
 
   @ApiProperty({ example: 2025 })
-  @IsNotEmpty()
-  @IsNumber()
   year: number;
 }
